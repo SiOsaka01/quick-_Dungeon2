d529b04c452e125fb8577cbc7f00e15b56a1ba95
Simon Osaka <59322145+SiOsaka01@users.noreply.github.com>
2023-06-05 09:34:09 +0100

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,34 @@
+//Global Variables set assignment values to datatypes 
+//let userName=prompt("Please enter your name!","Hero"); //string
+let score=0; //integer
+let timeTaken=0 //integer
+let apple=false; //boolean
+let key=false; //boolean
+let doorOpen=false; //boolean
+let facing="north"; //string
+let mpr="A1"; //string
+
+
+function moving(){
+//get ref to objects which will display gfx or values
+let tvp=document.getElementById("splash"); //splash is the graphic viewer element
+alert("the image viewer is called "+tvp);
+//tvp.style.src="";
+
+// how to change an image from a button click event? selected_img_element.src = " new link or address ";
+
+}
+
+function startGame(){
+//show door animation
+document.getElementById("").src=""
+
+//start timer. when it gets to 5 seconds, call beginGame function then show the dungeon gui.
+
+alert("Your game has started!");
+}
+
+function useConsole(){
+
+
+}
